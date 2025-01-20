546fdcedd20db820c81f7f300193940082ff0bb1
Eli Pacheco Hoyos <eph_074@hotmail.com>
2025-01-20 11:31:51 -0500

    Create Xcode Project

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Eli Pacheco Hoyos on 20/01/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
